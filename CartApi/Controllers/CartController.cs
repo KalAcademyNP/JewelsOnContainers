90b7ca7d7bde5353ce83e0f0b66a94d434ed5756
Kal <info@kalacademy.org>
2019-07-15 15:37:16 -0700

    Cart API

---
+++ CartApi/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CartApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CartApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+
+    public class CartController : Controller
+    {
+        private ICartRepository _repository;
+        public CartController(ICartRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/v1/cart/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Cart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get(string id)
+        {
+            var basket = await _repository.GetCartAsync(id);
+
+            return Ok(basket);
+        }
+
+        // POST api/v1/cart
+        [HttpPost]
+
+        [ProducesResponseType(typeof(Cart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Post([FromBody]Cart value)
+        {
+            var basket = await _repository.UpdateCartAsync(value);
+
+            return Ok(basket);
+        }
+
+        // DELETE api/v1/cart/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            //_logger.LogInformation("Delete method in Cart controller reached");
+            _repository.DeleteCartAsync(id);
+
+
+        }
+    }
+}
---
+++ CartApi/Startup.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using CartApi.Infrastructure.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CartApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<ConnectionMultiplexer>(sp =>
+            {
+                //var settings = sp.GetRequiredService<IOptions<CartSettings>>().Value;
+                var configuration = ConfigurationOptions.Parse(Configuration["ConnectionString"], true);
+                //resolving via dns before connecting
+                configuration.ResolveDns = true;
+                configuration.AbortOnConnectFail = false;
+
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+            ConfigureAuthService(services);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Basket HTTP API",
+                    Version = "v1",
+                    Description = "The Basket Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+                options.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = $"{Configuration.GetValue<string>("IdentityUrl")}/connect/authorize",
+                    TokenUrl = $"{Configuration.GetValue<string>("IdentityUrl")}/connect/token",
+                    Scopes = new Dictionary<string, string>()
+                    {
+                        { "basket", "Basket Api" }
+                    }
+                });
+
+                options.OperationFilter<AuthorizeCheckOperationFilter>();
+
+            });
+
+        }
+
+        private void ConfigureAuthService(IServiceCollection services)
+        {
+            // prevent from mapping "sub" claim to nameidentifier.
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            var identityUrl = Configuration.GetValue<string>("IdentityUrl");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = identityUrl;
+                options.RequireHttpsMetadata = false;
+                options.Audience = "basket";
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
